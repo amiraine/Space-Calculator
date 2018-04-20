2837ee94590cf3fab6871ec812543ef404a40ad7
Ami Cooper <amiraine.cooper@gmail.com>
2018-04-20 09:47:57 -0700

    write mars and jupiter age functions, pass specs

--- spec/space-spec.js
+++ spec/space-spec.js
@@ -2,15 +2,13 @@ import { Age } from "./../src/Age.js";
 
 describe('Age', function() {
   let todaysDate;
-  let testBirthday;
   let newAge;
-
+  //reset variables
   beforeEach(function() {
     todaysDate = new Date();
-    testBirthday = new Date(24,1994,0,1);
     newAge = new Age(1);
   });
-
+  //tests
   it('should return age converted to seconds', function(){
     expect(newAge.inSeconds()).toEqual(31536000);
     console.log(newAge.age);
@@ -18,4 +16,14 @@ describe('Age', function() {
   it('should return age converted to Mercury years', function(){
     expect(newAge.inMercury()).toEqual(0.24);
   });
+  it('should return age converted to Venus years', function(){
+    expect(newAge.inVenus()).toEqual(0.62);
+  });
+  it('should return age converted to Mars years', function(){
+    expect(newAge.inMars()).toEqual(1.88);
+  });
+  it('should return age converted to Jupiter years', function(){
+    expect(newAge.inJupiter()).toEqual(11.86);
+  });
 });
+//specs 1-5 passed
--- src/Age.js
+++ src/Age.js
@@ -12,6 +12,21 @@ class Age {
     mercuryValue = this.age * 0.24;
     return mercuryValue
   }
+  inVenus(){
+    let venusValue;
+    venusValue = this.age * 0.62;
+    return venusValue
+  }
+  inMars(){
+    let marsValue;
+    marsValue = this.age * 1.88;
+    return marsValue
+  }
+  inJupiter(){
+    let jupiterValue;
+    jupiterValue = this.age * 11.86;
+    return jupiterValue
+  }
 }
 
 export { Age };
