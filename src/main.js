78fde5bde4ffa175837f6d8f2506f419287a6178
Ami Cooper <amiraine.cooper@gmail.com>
2018-04-20 16:13:50 -0700

    finalize styles, check for additional bugs or errors

--- src/main.js
+++ src/main.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
     secoAge = newUser.inSeconds();
     secSpec = newDateOf.differenceSec(yearNow,monthNow,dayNow);
     mercAge = newUser.inMercury();
-    venuAge = newUser.inVenus();
+    venuAge = newUser.inVenus().toFixed(2);
     marsAge = newUser.inMars();
     jupiAge = newUser.inJupiter();
     lifeExpDiff = newUser.lifeExpect(raceInput);
@@ -55,7 +55,42 @@ $(document).ready(function() {
     $('#life-expect-return').text(lifeExpDiff);
     //
     $(".cardMiddle").slideDown();
+    $(".cardTwo").slideDown();
     $(".cardOne").slideUp();
+  }); // adds text to results
+  $("img#earthResult").on('click',function(){
+    $('#results').show();
+    $('#earth').fadeIn(500);
+    $('#yearsec').fadeIn(500);
+  }); //shows earth result on clicking the earth icon
+  $("img#mercuryResult").on('click',function(){
+    $('#results').show();
+    $('#mercury').fadeIn(500);
   });
-  
+  $("img#venusResult").on('click',function(){
+    $('#results').show();
+    $('#venus').fadeIn(500);
+  });
+  $("img#marsResult").on('click',function(){
+    $('#results').show();
+    $('#mars').fadeIn(500);
+  });
+  $("img#jupiterResult").on('click',function(){
+    $('#results').show();
+    $('#jupiter').fadeIn(500);
+  });
+  $("img#deathResult").on('click',function(){
+    $('#results').show();
+    $('#lifeexp').fadeIn(500);
+  });
+  $("img#everythingResult").on('click',function(){
+    $('#results').show();
+    $('#earth').show();
+    $('#yearsec').show();
+    $('#mercury').show();
+    $('#venus').show();
+    $('#mars').show();
+    $('#jupiter').show();
+    $('#lifeexp').show();
+  });
 });
